ff2114dcf8b9fe26783bc66839ebb51b78e26c08
Vladimir Ivanenko <hi@vldmr.me>
2021-09-28 08:54:08 +0300

    debug

--- worker.js
+++ worker.js
@@ -1,5 +1,21 @@
 function hello(r) {
-    r.return(200, "Hello world!");
+    // r.return(200, `http://${r.args.url}`);
+
+    r.return(200, JSON.stringify({
+        version: njs.version,
+        nginxVersion: r.variables.nginx_version
+    }));
+
+    // r.subrequest(`http://rwandanrecords.com/`, {method: 'GET'}, (res) => {
+    //     r.return(res.status, res.responseBody);
+    // }, (err) => {
+    //     r.return(501, JSON.stringify(err));
+    // });
+
+    // njx.fetch(`http://rwandanrecords.com/`)
+    //     .then(reply => reply.text())
+    //     .then(body => r.return(200, body))
+    //     .catch(e => r.return(501, e.message));
 }
 
 export default {hello};
