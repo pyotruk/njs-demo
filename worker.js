cc7ab58524944d35253c93d533229555f003ae23
Vladimir Ivanenko <hi@vldmr.me>
2021-09-29 16:13:58 +0300

    example works now

--- worker.js
+++ worker.js
@@ -1,21 +1,13 @@
 function hello(r) {
-    // r.return(200, `http://${r.args.url}`);
+    // r.return(200, JSON.stringify({
+    //     njsModuleVersion: njs.version,
+    //     nginxVersion: r.variables.nginx_version
+    // }));
 
-    r.return(200, JSON.stringify({
-        njsModuleVersion: njs.version,
-        nginxVersion: r.variables.nginx_version
-    }));
-
-    // r.subrequest(`http://rwandanrecords.com/`, {method: 'GET'}, (res) => {
-    //     r.return(res.status, res.responseBody);
-    // }, (err) => {
-    //     r.return(501, JSON.stringify(err));
-    // });
-
-    // njx.fetch(`http://rwandanrecords.com/`)
-    //     .then(reply => reply.text())
-    //     .then(body => r.return(200, body))
-    //     .catch(e => r.return(501, e.message));
+    ngx.fetch('http://nginx.org/')
+        .then(reply => reply.text())
+        .then(body => r.return(200, body))
+        .catch(e => r.return(501, e.message));
 }
 
 export default {hello};
